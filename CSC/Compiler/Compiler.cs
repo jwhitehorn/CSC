de205137d10730b451e07b5bea3d34f518dab5f3
Jason Whitehorn <jason.whitehorn@gmail.com>
2021-02-20 08:16:50 -0600

    Customizable Run method & better error handling

--- CSC/Compiler/Compiler.cs
+++ CSC/Compiler/Compiler.cs
@@ -1,24 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 
 namespace CSC.Compiler {
-    public class Compiler {
+    public class Compiler : IAntlrErrorListener<int>, IAntlrErrorListener<IToken> {
 
         private CSharpListenerImpl _listener;
+        private List<string> _errors;
 
         public Compiler() {
             _listener = new CSharpListenerImpl();
+            _errors = new List<string>();
         }
 
 
         public void Compile(string src) {
             var inputStream = new AntlrInputStream(src);
             CSharpLexer lexer = new CSharpLexer(inputStream);
+            lexer.RemoveErrorListeners();
+            lexer.AddErrorListener(this);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             CSharpParser parser = new CSharpParser(tokens);
+            parser.RemoveErrorListeners();
+            parser.AddErrorListener(this);
 
             IParseTree tree = parser.source_file();
             ParseTreeWalker walker = new ParseTreeWalker();
@@ -31,9 +38,13 @@ namespace CSC.Compiler {
         }
 
         public object Run(string[] args) {
+            return Run("Program", "Main", args);
+        }
+
+        public object Run(string klass, string method, string[] args) {
             var program = this.Finish();
-            var entryClassType = program.GetType("Program");
-            return entryClassType.InvokeMember("Main",
+            var entryClassType = program.GetType(klass);
+            return entryClassType.InvokeMember(method,
                                                 BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod,
                                                 null,
                                                 null,
@@ -63,5 +74,18 @@ namespace CSC.Compiler {
             return compiler.Run(args);
         }
 
+        public bool HasErrors {
+            get {
+                return _errors.Count > 0;
+            }
+        }
+
+        void IAntlrErrorListener<int>.SyntaxError(TextWriter output, IRecognizer recognizer, int offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e) {
+            _errors.Add(output.ToString());
+        }
+
+        void IAntlrErrorListener<IToken>.SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e) {
+            _errors.Add(output.ToString());
+        }
     }
 }
